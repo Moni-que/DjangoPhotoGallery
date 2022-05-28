2a74d14d24470d5fa33f8ecc754002ffe9dde730
moni-que <monicahjustus@gmail.com>
2022-05-28 14:18:31 +0300

    add all_photos view function as the base view function

--- PhotoGallery/urls.py
+++ PhotoGallery/urls.py
@@ -1,6 +1,12 @@
 from django.urls import re_path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns=[
-    re_path('^$',views.search_results,name = 'search'),
+    re_path(r'^$',views.all_photos,name='newsToday'),
+    # re_path(r'^search/',views.search_results,name = 'search_results'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
