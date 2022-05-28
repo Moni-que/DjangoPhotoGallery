28b78a60df0642c84c5b0a20338790080a61aca7
moni-que <monicahjustus@gmail.com>
2022-05-28 13:09:32 +0300

    add search result function

--- PhotoGallery/views.py
+++ PhotoGallery/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http  import HttpResponse
 import datetime as dt
 
 # Create your views here.
-def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+# def welcome(request):
+#     return HttpResponse('Welcome to the Moringa Tribune')
 
+def search_results(request):
+    return render(request, 'all-images/search.html')
