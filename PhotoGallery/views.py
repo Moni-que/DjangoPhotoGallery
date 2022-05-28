acf0cd4f075b6840342af6c37273c3112524d355
moni-que <monicahjustus@gmail.com>
2022-05-28 14:20:49 +0300

    create function

--- PhotoGallery/views.py
+++ PhotoGallery/views.py
@@ -1,3 +1,4 @@
+from urllib import request
 from django.shortcuts import render, redirect
 from django.http  import HttpResponse
 import datetime as dt
@@ -6,5 +7,8 @@ import datetime as dt
 # def welcome(request):
 #     return HttpResponse('Welcome to the Moringa Tribune')
 
-def search_results(request):
-    return render(request, 'all-images/search.html')
+# def search_results(request):
+#     return render(request, 'all-images/search.html')
+
+def all_photos(request):
+    return render(request, 'all_images/gallery.html')
