79ee3b89de1a8cf4a3b7296b8137109ba193f77f
moni-que <monicahjustus@gmail.com>
2022-05-29 14:25:41 +0300

    add category class

--- PhotoGallery/models.py
+++ PhotoGallery/models.py
@@ -1,18 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
 class Image(models.Model):
-    # image = models.ImageField(upload_to ='/')
+    image = models.ImageField(upload_to ='', default='')
     image_name = models.CharField(max_length =30)
-    image_description = models.TextField(max_length=150)
-    image_category = models.CharField(max_length=30)
-    image_location = models.CharField(max_length=20)
+    image_description = models.TextField(max_length=500)
+    image_category = models.ForeignKey(Category, on_delete=models.CASCADE,)
+    # image_location = models.CharField(max_length=20)
 
-
-
-# class Location(models.Model):
-
-
-
-# class Category(models.Model):
-
+    def __str__(self):
+        return self.image_description
