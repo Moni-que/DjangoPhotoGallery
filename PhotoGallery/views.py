fa48b17a47c098c5bf8c42389c31f663ad86403a
moni-que <monicahjustus@gmail.com>
2022-05-29 17:56:26 +0300

    add media template

--- PhotoGallery/views.py
+++ PhotoGallery/views.py
@@ -1,5 +1,3 @@
-from multiprocessing import context
-from unicodedata import category
 from django.shortcuts import render, redirect
 from django.http  import HttpResponse,Http404
 import datetime as dt
