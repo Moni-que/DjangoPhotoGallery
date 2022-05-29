087b6e26e2c6b7a01e8e3b862e3238eb4df9c88b
moni-que <monicahjustus@gmail.com>
2022-05-29 10:06:18 +0300

    create a model class Image

--- PhotoGallery/models.py
+++ PhotoGallery/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    # image = models.ImageField(upload_to ='/')
+    image_name = models.CharField(max_length =30)
+    image_description = models.TextField(max_length=150)
+    image_category = models.CharField(max_length=30)
+    image_location = models.CharField(max_length=20)
+
+
+
+# class Location(models.Model):
+
+
+
+# class Category(models.Model):
+
