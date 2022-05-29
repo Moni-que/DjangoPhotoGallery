0f3bf81a45bd0d6b60036c8515897f6805b160e3
moni-que <monicahjustus@gmail.com>
2022-05-29 13:40:50 +0300

    remove date

--- PhotoGallery/views.py
+++ PhotoGallery/views.py
@@ -8,21 +8,8 @@ def all_photos(request):
     date = dt.date.today()
     return render(request, 'all_images/gallery.html', {"date": date})
 
-def photo_details(request,past_date):
-
-    try:
-        # Converts data from the string Url
-        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
-
-    except ValueError:
-        # Raise 404 error when ValueError is thrown
-        raise Http404()
-        assert False
-
-    if date == dt.date.today():
-        return redirect(all_photos)
-
-    return render(request, 'all_images/photo.html', {"date": date})
+def photo_details(request):
+    return render(request, 'all_images/photo.html')
 
 
 def search_results(request):
