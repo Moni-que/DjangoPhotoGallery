0c417059314a15dc072b166d84126c15a0b952d4
moni-que <monicahjustus@gmail.com>
2022-05-29 17:54:19 +0300

    add image category

--- PhotoGallery/views.py
+++ PhotoGallery/views.py
@@ -1,12 +1,17 @@
+from multiprocessing import context
+from unicodedata import category
 from django.shortcuts import render, redirect
 from django.http  import HttpResponse,Http404
 import datetime as dt
+from .models import Category, Image
 
 # Create your views here.
 
 def all_photos(request):
-    date = dt.date.today()
-    return render(request, 'all_images/gallery.html', {"date": date})
+    # date = dt.date.today()
+    categories = Category.objects.all()
+    context = { 'categories':  categories} 
+    return render(request, 'all_images/gallery.html', context)
 
 def photo_details(request):
     return render(request, 'all_images/photo.html')
