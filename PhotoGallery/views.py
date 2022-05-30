294b0833a753dc32fa866d8e5b7fb470949f7f43
moni-que <monicahjustus@gmail.com>
2022-05-31 01:59:23 +0300

    add if statements to add a new category with photos

--- PhotoGallery/views.py
+++ PhotoGallery/views.py
@@ -1,3 +1,4 @@
+from venv import create
 from django.shortcuts import render, redirect
 from django.http  import HttpResponse,Http404
 import datetime as dt
@@ -23,5 +24,17 @@ def search_results(request):
 
 def add_photo(request):
     categories = Category.objects.all()
+    if request.method == 'POST':
+        data = request.POST
+        image = request.FILES.get('image')
+        if data['category'] != 'none':
+            category = Category.objects.get(id=data['category'])
+        elif data['new_category'] != '':
+            category, created = Category.objects.get_or_create(none=data['new_category'])
+        else:
+             category = None
+        photo = Image.objects.create(category = category, description = data['image_description'], image =image)
+
+        return redirect('gallery')
     context = { 'categories':  categories}
     return render(request, 'all_images/add.html', context)
