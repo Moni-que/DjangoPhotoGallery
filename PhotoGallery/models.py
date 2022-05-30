72ea4af8b5fdb604d71578e5bc6f15bebb464aed
moni-que <monicahjustus@gmail.com>
2022-05-30 13:19:46 +0300

    remove commented code

--- PhotoGallery/models.py
+++ PhotoGallery/models.py
@@ -14,7 +14,6 @@ class Image(models.Model):
     image_name = models.CharField(max_length =30)
     image_description = models.TextField(max_length=500)
     image_category = models.ForeignKey(Category, on_delete=models.CASCADE,)
-    # image_location = models.CharField(max_length=20)
 
     def __str__(self):
         return self.image_description
