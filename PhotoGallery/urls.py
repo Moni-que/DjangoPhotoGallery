b2562ed6c9328731f7eb38fe809bec048a9e94be
moni-que <monicahjustus@gmail.com>
2022-05-30 13:15:16 +0300

    addd right path

--- PhotoGallery/urls.py
+++ PhotoGallery/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns=[
     re_path(r'^$',views.all_photos,name='gallery'),
-    re_path(r'^details/', views.photo_details,name='photo_details'),
+    re_path(r'^details/(\d+)', views.photo_details,name='photo_details'),
     re_path(r'^search/',views.search_results,name='search_results'),
     re_path(r'^add/',views.add_photo,name='add'),
 ]
