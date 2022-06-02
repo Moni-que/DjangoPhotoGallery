928e7973d49718827f2f72110721bd4874f2f43f
moni-que <monicahjustus@gmail.com>
2022-06-02 16:11:34 +0300

    add tests

--- PhotoGallery/models.py
+++ PhotoGallery/models.py
@@ -9,6 +9,11 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
+    def save_image(self):
+        self.save()
+        
+
 # class Location(models.Model):
 #     name = models.CharField(max_length=30)
 
@@ -22,9 +27,13 @@ class Image(models.Model):
     image_category = models.ForeignKey(Category, on_delete=models.CASCADE,)
     # image_location = models.ForeignKey(Location, on_delete=models.CASCADE, default='')
 
+
+    def save_category(self):
+        self.save()
+
     @classmethod
     def search_by_category(cls, category_term):
-        gallery = cls.objects.filter(name__name=category_term)
+        gallery = cls.objects.filter(name__icontains=category_term)
         return gallery
 
 
--- PhotoGallery/tests.py
+++ PhotoGallery/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from .models import Image,Category
+import datetime as dt
 
 # Create your tests here.
+class ImageTestClass(TestCase):
+
+    #set up method
+    def setUp(self):
+        self.myimage = Image(image_name = 'Travel', image_description = 'This is my travel image')
+
+     #Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.myimage,Image))
+
+    #Testing Save Method
+    def test_save_method(self):
+        self.myimage.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
+
+
+class CategoryTestClass(TestCase):
+    #set up method
+    def setUp(self):
+        # self.myimage = Image(image_name = 'Travel', image_description = 'This is my travel image')
+
+        self.new_category = Category(name = 'testing')
+        self.new_category.save()
+
+
+    def tearDown(self):
+        Image.objects.all().delete()
+        Category.objects.all().delete()
